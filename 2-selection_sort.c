c23adcb5c18cccff4f662c2ca97782fb700f8382
Lonzo Rust <rustlonzo@gmail.com>
2021-08-26 15:23:17 +0000

    Corrected a syntax error.

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -18,7 +18,7 @@ void selection_sort(int *array, size_t size)
 		min = i;
 		for (k = i + 1; k < size; k++)
 		{
-			if (array[k] < array[min]
+			if (array[k] < array[min])
 				min = k
 		}
 		if (min != i)
